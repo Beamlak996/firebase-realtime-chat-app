7b78e2d83b8e300fd34b0b0885a6b0b8338dce9f
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-12-27 22:50:46 -0900

    Setting Up Firebase Done

---
+++ src/firebase-config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyABuHvqf-4Q2JCk0uaLdxxHzW9OW2GZAY4",
+  authDomain: "chatapp-59ff1.firebaseapp.com",
+  projectId: "chatapp-59ff1",
+  storageBucket: "chatapp-59ff1.appspot.com",
+  messagingSenderId: "29055585039",
+  appId: "1:29055585039:web:1bdba3797227b2b5a24c7a",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const  provider = new GoogleAuthProvider()
